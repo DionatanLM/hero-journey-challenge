527f03c7b3b24d67d48be42df00df609fd433a59
dionatanlm <dionatanlm12271@gmail.com>
2023-10-30 21:47:33 -0300

    fix: remove axios

--- frontend/src/services/hero.service.js
+++ frontend/src/services/hero.service.js
@@ -1,11 +1,20 @@
-import axios from 'axios'
-
 const getAllHeroes = async () => {
-  const result = await axios.get(
-    'http://homologacao3.azapfy.com.br/api/ps/metahumans'
-  )
+  try {
+    const response = await fetch(
+      'http://homologacao3.azapfy.com.br/api/ps/metahumans'
+    )
 
-  return result.data
+    if (!response.ok) {
+      throw new Error('Erro ao obter os heróis')
+    }
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    // Trate os erros aqui conforme necessário
+    console.error('Erro ao obter os heróis:', error)
+    throw error
+  }
 }
 
 const heroService = {
